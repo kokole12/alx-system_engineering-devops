a7b0d307dd58026ffd04b69d01e9196b3e11979d
kokole12 <kokoleismail886@gmail.com>
2023-03-24 14:00:05 +0300

    creating a file in puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# this puppet file create a new file /temp
-file{'/temp/chool':
-  ensure => 'file',
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
-  content => 'I love Puppet'
+# This manifest creates a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
