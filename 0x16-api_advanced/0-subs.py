b1b4b9e6d5b7bd252d905dbf56fe4a83bbce074e
kokole12 <kokoleismail886@gmail.com>
2023-05-10 00:49:45 +0300

    task 0 fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"user-agent": "REDDITAPI/0.0.1"}
     response = requests.get(url, headers=headers, allow_redirects=False)
